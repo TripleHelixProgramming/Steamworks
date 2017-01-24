822eeae80f90d096ff5ca405ce171c19b5338d51
vagee <vagee@WENDYS-LAPTOP>
2017-01-24 08:29:25 -0500

    Removed commented out code from serial port attempt.

--- Steamworks/src/org/usfirst/frc/team2363/robot/subsystems/Pixy.java
+++ Steamworks/src/org/usfirst/frc/team2363/robot/subsystems/Pixy.java
@@ -19,24 +19,10 @@ public class Pixy extends Subsystem {
     	OFF
     }
     public static I2C pixyi2c;
-
-/*	private static SerialPort pixy_port;
-	// private static Port port = Port.kMXP;   
-	// NavX is on the port above, so using kOnBoard
-	private static Port port = Port.kOnboard;
-	private static PixyPacket[] packets;
-	PixyException pExc;
-	private static String print;
-*/
 	public PixyState state;
 
 	public Pixy() {
-		/*
-		pixy_port = new SerialPort(19200, port);
-		pixy_port.setReadBufferSize(14);
-		packets = new PixyPacket[7];
-		pExc = new PixyException(print);
-		*/
+		
 		pixyi2c = new I2C(I2C.Port.kOnboard, 0x54);
 		state = PixyState.OFF;
 	}
@@ -62,29 +48,21 @@ public class Pixy extends Subsystem {
     	SmartDashboard.putNumber("height", Target.Height);
     	SmartDashboard.putNumber("Raw 5", Target.Sig);
 	}
+	
 	public void off() {
 		state = PixyState.OFF;
 		SmartDashboard.putNumber("Pixy", 0);
 	}
+	
 	public boolean isOn() {
 		return (state == PixyState.ON);
 	}
+	
 	public boolean isOff() {
 		return (state == PixyState.OFF);
 	}
 	
-	/*
-	 * public void pixyReset(){
-	
-		pixy_port.reset();
-	}
-	 */
-	
-	//This method parses raw data from the pixy_port into readable integers
-/*	public int cvt(byte upper, byte lower) {
-		return (((int)upper & 0xff) << 8) | ((int)lower & 0xff);
-	}
-*/	
+	//This method parses raw data from the I2C port into Pixy Packet's X , Y, Width, Height, Signature.
 	public PixyPacket readPixyPacket(){
 		byte[] pixyValues = new byte[64];
 		pixyValues[0] = (byte) 0b01010101;
@@ -116,59 +94,5 @@ public class Pixy extends Subsystem {
 		}
 		return packet;
 	}
-	//This method gathers data, then parses that data, and assigns the ints to global variables
-	/* public PixyPacket readPacket(int Signature) throws PixyException {
-		int Checksum;
-		int Sig;
-		byte[] rawData = new byte[32];
-		
-		try{
-			rawData = pixy_port.read(32);
-		} catch (RuntimeException e){
-		}
-		
-		if(rawData.length < 32){
-			System.out.println("byte array length is broken");
-			return null;
-		}
-		
-		for (int i = 0; i <= 16; i++) {
-			int syncWord = cvt(rawData[i+1], rawData[i+0]); //Parse first 2 bytes
-			if (syncWord == 0xaa55) { //Check is first 2 bytes equal a "sync word", which indicates the start of a packet of valid data
-				syncWord = cvt(rawData[i+3], rawData[i+2]); //Parse the next 2 bytes
-				if (syncWord != 0xaa55){ //Shifts everything in the case that one syncword is sent
-					i -= 2;
-				}
-				
-				//This next block parses the rest of the data
-				Checksum = cvt(rawData[i+5], rawData[i+4]);
-				Sig = cvt(rawData[i+7], rawData[i+6]);
-				if(Sig <= 0 || Sig > packets.length){
-					break;
-				}
-				packets[Sig - 1] = new PixyPacket();
-				packets[Sig - 1].X = cvt(rawData[i+9], rawData[i+8]);
-				packets[Sig - 1].Y = cvt(rawData[i+11], rawData[i+10]);
-				packets[Sig - 1].Width = cvt(rawData[i+13], rawData[i+12]);
-				packets[Sig - 1].Height = cvt(rawData[i+15], rawData[i+14]);
-				
-				//Checks whether the data is valid using the checksum *This if block should never be entered*
-				if (Checksum != Sig + packets[Sig - 1].X + packets[Sig - 1].Y + packets[Sig - 1].Width + packets[Sig - 1].Height) {
-					packets[Sig - 1] = null;
-					throw pExc;
-				}
-				break;
-			}
-		}
-		//Assigns our packet to a temp packet, then deletes data so that we dont return old data
-		PixyPacket pkt = packets[Signature - 1];
-		packets[Signature - 1] = null;
-		return pkt;
-	}
-	
-	public void updateSmartDash () {
-		
-	}
-	*/
 }
 
